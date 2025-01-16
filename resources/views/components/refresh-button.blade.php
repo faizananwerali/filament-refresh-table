3c644d6c60bf35128f255bc61d688072b138ffc5
faizananwerali <faizanawer2011@gmail.com>
2025-01-16 11:44:02 +0400

    loading icon issue fixes.

--- resources/views/components/refresh-button.blade.php
+++ resources/views/components/refresh-button.blade.php
@@ -11,9 +11,18 @@
             x-on:click="$wire.call('$refresh')"
     />
 
+{{--    <x-filament::loading-indicator--}}
+{{--            x-cloak--}}
+{{--            wire:loading.delay--}}
+{{--            class="h-5 w-5"--}}
+{{--    />--}}
     <x-filament::loading-indicator
-            x-cloak
-            wire:loading.delay
-            class="h-5 w-5"
+            :attributes="
+            \Filament\Support\prepare_inherited_attributes(
+                new \Illuminate\View\ComponentAttributeBag([
+                    'wire:loading.delay.' . config('filament.livewire_loading_delay', 'default') => '',
+                ])
+            )->class(['h-5 w-5'])
+        "
     />
 </div>
